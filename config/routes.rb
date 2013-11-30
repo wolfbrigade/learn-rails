1f5d79e130e971a3bc652cf97eb7dc1b5020c906
John Persson <john.persson@hyperisland.se>
2013-11-30 17:07:48 +0100

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 LearnRails::Application.routes.draw do
-  post 'contact', to: 'contacts#process_form'
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
 end
   # The priority is based upon order of creation: first created -> highest priority.
